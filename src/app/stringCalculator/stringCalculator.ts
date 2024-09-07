9bdcf4ac4c24aa9f7fc67f50a39f123fa8391ef5
RYTHM RANA <53206042+rythmrana2@users.noreply.github.com>
2024-09-07 23:51:42 +0530

    refactored the code after all tests were passing

--- src/app/stringCalculator/stringCalculator.ts
+++ src/app/stringCalculator/stringCalculator.ts
@@ -1,8 +1,8 @@
 export class StringCalculator {
     Add(numbers: string): number {
         let answer = parseInt(numbers);
-        let parsedNumbers = numbers.split(',');
-        if (parsedNumbers.length > 1) {
+        if (!isNaN(answer)) {
+            let parsedNumbers = numbers.split(',');
             answer = parsedNumbers.reduce((accumulator, currentValue) => {
                 return accumulator += parseInt(currentValue);
             }, 0);
