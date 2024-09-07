c0067cd11a6ee9cf4dcb67fb7680debb0e19e299
RYTHM RANA <53206042+rythmrana2@users.noreply.github.com>
2024-09-08 01:10:21 +0530

    passed changed delimiter test no 8

--- src/app/stringCalculator/stringCalculator.ts
+++ src/app/stringCalculator/stringCalculator.ts
@@ -1,8 +1,14 @@
 export class StringCalculator {
     Add(numbers: string): number {
+        let correctedInput = numbers;
         let answer = 0;
         if (numbers !== "") {
-            let alteredNumbers = numbers.replace(new RegExp('\n', 'g'), ',');
+            let delimiter = '\n';
+            if (numbers.startsWith("//")) {
+                delimiter = '[' + delimiter + numbers[2] + ']';
+                correctedInput = correctedInput.slice(4);
+            }
+            let alteredNumbers = correctedInput.replace(new RegExp(delimiter, 'g'), ',');
             let toBeParsedNumbers = alteredNumbers.split(',');
             answer = toBeParsedNumbers.reduce((accumulator, currentValue) => {
                 return accumulator += parseInt(currentValue);
