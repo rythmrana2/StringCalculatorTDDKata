f41464d3821dc55a211a0a1c41945b6c83b646df
RYTHM RANA <53206042+rythmrana2@users.noreply.github.com>
2024-09-08 01:43:35 +0530

    refactored code

--- src/app/stringCalculator/stringCalculator.ts
+++ src/app/stringCalculator/stringCalculator.ts
@@ -6,18 +6,25 @@ export class StringCalculator {
             let delimiter = '\n';
             if (correctedInput.startsWith("//")) {
                 delimiter = '[' + delimiter + correctedInput[2] + ']';
+                //slicing as first 4 characters are delimiter information
                 correctedInput = correctedInput.slice(4);
             }
             let alteredNumbers = correctedInput.replace(new RegExp(delimiter, 'g'), ',');
-            let toBeParsedNumbers = alteredNumbers.split(',');
-            answer = toBeParsedNumbers.reduce((accumulator, currentValue) => {
-                let parsedNumber = parseInt(currentValue);
-                if (parsedNumber < 0) {
-                    throw new Error("negatives not allowed " + parsedNumber);
-                }
-                return accumulator += parsedNumber;
-            }, 0);
+            answer = this.getAnswerFromString(alteredNumbers);
         }
         return answer;
+    }
+
+    //helper function has decreased main function size and separated logic that is not required to be shown in main function
+    getAnswerFromString(alteredNumbers) {
+        let toBeParsedNumbers = alteredNumbers.split(',');
+        let solution = toBeParsedNumbers.reduce((accumulator, currentValue) => {
+            let parsedNumber = parseInt(currentValue);
+            if (parsedNumber < 0) {
+                throw new Error("negatives not allowed " + parsedNumber);
+            }
+            return accumulator += parsedNumber;
+        }, 0);
+        return solution;
     }
 }
