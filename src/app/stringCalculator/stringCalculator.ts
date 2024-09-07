e217e7ffe36ae0ce12fcf798054c82a0658b3e0d
RYTHM RANA <53206042+rythmrana2@users.noreply.github.com>
2024-09-08 00:16:57 +0530

    refactored code to make more readable

--- src/app/stringCalculator/stringCalculator.ts
+++ src/app/stringCalculator/stringCalculator.ts
@@ -1,14 +1,13 @@
 export class StringCalculator {
     Add(numbers: string): number {
-        let answer = parseInt(numbers);
-        if (!isNaN(answer)) {
+        let answer = 0;
+        if (numbers !== "") {
             let alteredNumbers = numbers.replace('\n', ',');
             let toBeParsedNumbers = alteredNumbers.split(',');
             answer = toBeParsedNumbers.reduce((accumulator, currentValue) => {
                 return accumulator += parseInt(currentValue);
             }, 0);
         }
-        //as logical OR returns second operand if both operands are falsy
-        return (answer || 0);
+        return answer;
     }
 }
