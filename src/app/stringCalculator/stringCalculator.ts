18625895b636eb5c3af255eb03b910caf0f3b56b
RYTHM RANA <53206042+rythmrana2@users.noreply.github.com>
2024-09-07 23:25:29 +0530

    all three tests pass

--- src/app/stringCalculator/stringCalculator.ts
+++ src/app/stringCalculator/stringCalculator.ts
@@ -1,6 +1,7 @@
 export class StringCalculator {
     Add(numbers: string): number {
         let answer = parseInt(numbers);
-        return answer;
+        //as logical OR returns second operand if both operands are falsy
+        return (answer || 0);
     }
 }
