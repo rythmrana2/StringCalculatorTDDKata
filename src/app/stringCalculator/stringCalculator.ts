150ce9ba7dcf34976d55cecb12ae32900489c900
RYTHM RANA <53206042+rythmrana2@users.noreply.github.com>
2024-09-08 01:11:46 +0530

    refactored code after passing tests

--- src/app/stringCalculator/stringCalculator.ts
+++ src/app/stringCalculator/stringCalculator.ts
@@ -2,10 +2,10 @@ export class StringCalculator {
     Add(numbers: string): number {
         let correctedInput = numbers;
         let answer = 0;
-        if (numbers !== "") {
+        if (correctedInput !== "") {
             let delimiter = '\n';
-            if (numbers.startsWith("//")) {
-                delimiter = '[' + delimiter + numbers[2] + ']';
+            if (correctedInput.startsWith("//")) {
+                delimiter = '[' + delimiter + correctedInput[2] + ']';
                 correctedInput = correctedInput.slice(4);
             }
             let alteredNumbers = correctedInput.replace(new RegExp(delimiter, 'g'), ',');
