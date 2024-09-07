f9c9bcf5954fbbae56cac0a4e49b159f1448c2bd
RYTHM RANA <53206042+rythmrana2@users.noreply.github.com>
2024-09-07 23:03:40 +0530

    tested if the class instance is getting created correctly

--- src/app/stringCalculator/stringCalculator.spec.ts
+++ src/app/stringCalculator/stringCalculator.spec.ts
@@ -1,3 +1,8 @@
-describe("StringCalculatorTddKata", () => {
+import { StringCalculator } from "./stringCalculator";
 
+describe("StringCalculatorTddKata", () => {
+    it("Class instance should get created", () => {
+        let stringCalculator = new StringCalculator();
+        expect(stringCalculator).toEqual(jasmine.any(StringCalculator));
+    });
 });
--- src/app/stringCalculator/stringCalculator.ts
+++ src/app/stringCalculator/stringCalculator.ts
@@ -1,3 +1,3 @@
-class StringCalculator {
+export class StringCalculator {
 
 }
